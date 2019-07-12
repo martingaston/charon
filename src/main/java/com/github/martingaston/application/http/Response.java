42134bda4570836592f083177ac5fb66d3389f3d
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2019-07-12 12:48:55 +0100

    Sprint1/head requests (#4)
    * add license and readme
    
    * add intellij files
    
    * add first tests for app
    
    * add first test for Connection
    
    * add first tests for awaiting a client connection
    
    * move request into client class and return to green
    
    * move connection implementation into constructor
    
    * add Sender class
    
    * add Receiver class
    
    * implement Sender and Receiver in app
    
    * implement Sender and Receiver into other classes
    
    * add receiveWhileReady and parse request in App
    
    * add tests for Request class
    
    * remove duplication from App and bring in Request object
    
    * refactor http request info in tests
    
    * refactor Request by extracting methods
    
    * extract Receiver methods for refactoring
    
    * rush to green for head request acceptance tests
    
    * move response logic into Response class
    
    * refactor bodytest to include check for length
    
    * add implementation to pass second & third acceptance tests
    
    * add response and verb for simple GET request
    
    * duplicate acceptance test locally

---
+++ src/main/java/com/github/martingaston/application/http/Response.java
@@ -0,0 +1,30 @@
+package com.github.martingaston.application.http;
+
+public class Response {
+    private Request request;
+
+    public Response(Request request) {
+        this.request = request;
+    }
+
+    public String respond() {
+        String response;
+
+        switch (request.method()) {
+            case GET:
+                response = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: 0\r\n\r\n";
+                break;
+            case POST:
+                response = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: " + request.bodyContentLength() + "\r\n\r\n" + request.body().toString();
+                break;
+            case HEAD:
+                response = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: " + request.bodyContentLength() + "\r\n\r\n";
+                break;
+            default:
+                response = "HTTP/1.1 405 Method Not Allowed\r\n\r\n";
+                break;
+        }
+
+        return response;
+    }
+}
