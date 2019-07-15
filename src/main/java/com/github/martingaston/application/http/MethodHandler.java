70783c782d6ee7344105d18a6573006317f819fd
Martin Gaston <squidman@gmail.com>
2019-07-15 16:26:04 +0100

    add MethodHandler class

---
+++ src/main/java/com/github/martingaston/application/http/MethodHandler.java
@@ -0,0 +1,33 @@
+package com.github.martingaston.application.http;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+public class MethodHandler {
+    private ConcurrentHashMap<Verbs, Handler> methods;
+
+    MethodHandler() {
+        methods = new ConcurrentHashMap<>();
+    }
+
+    void add(Verbs method, Handler handler) {
+        methods.putIfAbsent(method, handler);
+    }
+
+    Handler get(Verbs method) {
+        return methods.get(method);
+    }
+
+    boolean isValidMethod(Verbs method) {
+        return methods.containsKey(method);
+    }
+
+    String valid() {
+        return methods
+                .keySet()
+                .stream()
+                .map(Verbs::name)
+                .sorted()
+                .collect(Collectors.joining(", "));
+    }
+}
---
+++ src/test/java/com/github/martingaston/application/http/MethodHandlerTest.java
@@ -0,0 +1,44 @@
+package com.github.martingaston.application.http;
+
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("A MethodHandler class")
+class MethodHandlerTest {
+    private MethodHandler methodHandler;
+
+    @BeforeEach
+    void init() {
+        methodHandler = new MethodHandler();
+    }
+
+    @DisplayName("Returns true if a method exists")
+    @Test
+    void methodExists() {
+        methodHandler.add(Verbs.POST, new HandleEchoBody());
+        assertThat(methodHandler.isValidMethod(Verbs.POST)).isTrue();
+        assertThat(methodHandler.isValidMethod(Verbs.GET)).isFalse();
+    }
+
+    @DisplayName("Can output valid methods sorted alphabetically")
+    @Test
+    void knowsValidMethods() {
+        methodHandler.add(Verbs.POST, new HandleEchoBody());
+        methodHandler.add(Verbs.HEAD, new HandleEchoBody());
+        methodHandler.add(Verbs.GET, new HandleEchoBody());
+
+        assertThat(methodHandler.valid()).isEqualTo("GET, HEAD, POST");
+    }
+
+    @DisplayName("Does not overwrite existing methods once added")
+    @Test
+    void doesNotOverwrite() {
+        var request = new Request(new RequestLine(Verbs.POST, URI.from("/refactor_echo_body"), Version.V1POINT1), new Headers(), Body.from("It is a truth universally acknowledged..."));
+        methodHandler.add(Verbs.POST, req -> new Response(Status.OK, new Headers(), Body.from("ABC")));
+        methodHandler.add(Verbs.POST, req -> new Response(Status.OK, new Headers(), Body.from("DEF")));
+
+        Body body = methodHandler.get(Verbs.POST).handle(request).body();
+        assertThat(body).isEqualTo(Body.from("ABC"));
+    }
+}
