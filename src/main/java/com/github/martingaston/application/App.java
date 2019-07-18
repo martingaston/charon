e3cc7effa3dbd661f2a9262e521ef095a540542e
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2019-07-18 08:40:25 +0100

    Sprint2/deploy heroku (#11)
    * add Port class
    
    * add Port to Connection
    
    * check for port and update Procfile to use Java System Property

--- src/main/java/com/github/martingaston/application/App.java
+++ src/main/java/com/github/martingaston/application/App.java
@@ -2,6 +2,7 @@ package com.github.martingaston.application;
 
 import com.github.martingaston.application.http.*;
 import com.github.martingaston.application.transport.Connection;
+import com.github.martingaston.application.transport.Port;
 import com.github.martingaston.application.transport.Server;
 
 import java.io.IOException;
@@ -9,6 +10,8 @@ import java.io.IOException;
 class App {
     private Server connection;
     private Runner running;
+    private static final String PORT_PROPERTY = "app.port";
+    private static final int DEFAULT_PORT = 5000;
 
     public App(Server connection) {
         this(connection, new LiveRunning());
@@ -20,7 +23,8 @@ class App {
     }
 
     public static void main(String[] args) throws IOException {
-        var connection = new Connection(5000);
+        var port = new Port(getPortNumber());
+        var connection = new Connection(port);
         var app = new App(connection);
 
         app.listen();
@@ -35,6 +39,16 @@ class App {
 
             client.send(ResponseSender.from(response));
         }
+    }
+
+    private static int getPortNumber() {
+        var portProperty = System.getProperty(PORT_PROPERTY);
+
+        if(portProperty == null) {
+            return DEFAULT_PORT;
+        }
+
+        return Integer.parseInt(portProperty);
     }
 }
 
---
+++ src/test/java/com/github/martingaston/application/transport/PortTest.java
@@ -0,0 +1,19 @@
+package com.github.martingaston.application.transport;
+
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PortTest {
+    @Test
+    void createsOnRegisteredPort() {
+        var port = new Port(5000);
+        assertThat(port.get()).isEqualTo(5000);
+    }
+
+    @Test
+    void willNotCreateOnSystemPort() {
+        var thrown = catchThrowable(() -> new Port(80));
+        assertThat(thrown).hasMessageContaining("Invalid port");
+    }
+}
